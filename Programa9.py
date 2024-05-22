cff82f7d26ba3ac4fd3da4706f820fcd7996b0c0
michcordova <michelle_3128230483@utd.edu.mx>
2024-05-21 19:45:12 -0600

    Contraseña

---
+++ Programa9.py
@@ -0,0 +1,7 @@
+contraseña = input("Ingrese su contraseña: ")
+
+while contraseña != "poo123":
+    print("Contraseña incorrecta. Inténtelo nuevamente.")
+    contraseña = input("Ingrese su contraseña: ")
+
+print("Contraseña válida, bienvenido. ")
