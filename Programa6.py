3ef0e56c6e4d7aae16724e100008c96bce15b96e
michcordova <michelle_3128230483@utd.edu.mx>
2024-05-21 19:41:47 -0600

    Numero mayor

---
+++ Programa6.py
@@ -0,0 +1,10 @@
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+
+if numero1 > numero2:
+    print("El primer número es mayor que el segundo número")
+elif numero1 < numero2:
+    print("El segundo número es mayor que el primer número")
+else:
+    print("Ambos números son iguales.")
